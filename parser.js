485f4ba68243e61b16cacfbbe7aa91da8b1ae9c0
Haakasaurus <kramerhjohnson@gmail.com>
2020-01-11 11:33:48 -0800

    Got parser working for csv file.  Not all key values are accurate.

--- parser.js
+++ parser.js
@@ -3,3 +3,73 @@
 
 
 const fs = require('fs');
+
+// loop through all items in the csv
+// add each item to an array
+// when you reach a newline character,
+//     add the previous array to a larger array
+//     create a new array for the next line
+
+
+let release = [],
+    wantArr = [];
+
+// for (let i = 0; i < wantList.length; i++) {
+//     if (wantList[i] === '\n') {
+//         wantArr.push(release);
+//         release = [];
+//     } else {
+
+//     }
+// }
+
+function returnAllData(path) {
+
+    fs.readFile(path, 'utf-8', (err, allText) => {
+        if (err) throw err;
+
+        var allTextLines = allText.split(/\r\n|\n/);
+        var headers = allTextLines[0].split(',');
+        var lines = [];
+
+        for (var i = 1; i < allTextLines.length; i++) {
+            var data = allTextLines[i].split(',');
+
+
+            var tarr = [];
+            for (var j = 0; j < headers.length; j++) {
+                tarr.push(headers[j] + ":" + data[j]);
+            }
+            lines.push(tarr);
+
+        }
+        console.log(lines);
+        return lines;
+    });
+};
+
+function getArtists(path) {
+
+    fs.readFile(path, 'utf-8', (err, allText) => {
+        if (err) throw err;
+
+        var allTextLines = allText.split(/\r\n|\n/);
+        var headers = allTextLines[0].split(',');
+        var lines = [];
+
+        for (var i = 1; i < allTextLines.length; i++) {
+            var data = allTextLines[i].split(',');
+
+
+            var tarr = [];
+            for (var j = 0; j < headers.length; j++) {
+                tarr.push(headers[j] + ":" + data[j]);
+            }
+            lines.push(tarr);
+
+        }
+        for (let i = 0; i < lines.length; i++) {
+
+        }
+    });
+};
