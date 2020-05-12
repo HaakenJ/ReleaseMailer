35f594874f05420163baf1b3369dfc2295345a86
Haakasaurus <kramerhjohnson@gmail.com>
2020-05-11 19:42:09 -0700

    Changed to ES6 syntax

--- parser.js
+++ parser.js
@@ -53,23 +53,23 @@ function getArtists(path) {
     fs.readFile(path, 'utf-8', (err, allText) => {
         if (err) throw err;
 
-        var allTextLines = allText.split(/\r\n|\n/);
-        var headers = allTextLines[0].split(',');
-        var lines = [];
+        const allTextLines = allText.split(/\r\n|\n/);
+        const headers = allTextLines[0].split(',');
+        const lines = [];
 
-        for (var i = 1; i < allTextLines.length; i++) {
-            var data = allTextLines[i].split(',');
+        for (let i = 1; i < allTextLines.length; i++) {
+            const data = allTextLines[i].split(',');
 
 
-            var tarr = {};
-            for (var j = 0; j < headers.length; j++) {
-                tarr[headers[j]] = data[j];
+            const currentArtistObj = {};
+            for (let j = 0; j < headers.length; j++) {
+                currentArtistObj[headers[j]] = data[j];
             }
-            lines.push(tarr);
+            lines.push(currentArtistObj);
 
         }
 
-        let artists = [];
+        const artists = [];
         for (let i = 0; i < lines.length; i++) {
             artists.push(lines[i]['Artist']);
         }
@@ -78,8 +78,12 @@ function getArtists(path) {
     });
 };
 
-// returnAllData('./wantlist/wantlist.csv');
-// getArtists('./wantlist/wantlist.csv')
+function getKeyWords(artists) {
+    
+}
+
+returnAllData('./wantlist/wantlist.csv');
+getArtists('./wantlist/wantlist.csv')
 
 const artists = getArtists('./wantlist/wantlist.csv');
 
