325c4b8ab0be1f3cd0f5e4b248caa14f4ffb67a7
Haakasaurus <kramerhjohnson@gmail.com>
2020-05-14 18:25:00 -0700

    Adapated scrape to use a single url

--- scrape.js
+++ scrape.js
@@ -9,42 +9,35 @@ const Parser = require('./parser');
   @return array - array of object literals where each object is a matched link
   with a title and a link property.
 */
-function matchLinks(keywords) {
-  const urlArr = [
-    "https://old.reddit.com/r/vinylreleases",
-    "https://old.reddit.com/r/VinylReleases/?count=25&after=t3_ehlmpi",
-    "https://old.reddit.com/r/VinylReleases/?count=50&after=t3_eemh1e"
-  ];
 
-  keywords["lee"] = true;
+function matchLinks(keywords) {
+  const url = "https://old.reddit.com/r/vinylreleases";
   
   const results = [];
 
-  for (const url of urlArr) {
-    axios.get(url).then(response => {
-      const $ = cheerio.load(response.data);
-      // Loop through all the titles in the page
-      // Add title, link, and matched keyword if keyword is found in the title
-      $("a.title").each((iter, element) => {
+  axios.get(url).then(response => {
+    const $ = cheerio.load(response.data);
+    // Loop through all the titles in the page
+    // Add title, link, and matched keyword if keyword is found in the title
+    $("a.title").each((iter, element) => {
 
-        const title = $(element).text();
-        const link = $(element).attr("href");
-        const titleKeywords = title.split(" ");
+      const title = $(element).text();
+      const link = $(element).attr("href");
+      const titleKeywords = title.split(" ");
 
-        for (const word of titleKeywords) {
-          if (keywords[word] === true) {
-            console.log("Found a match");
-            const match = {
-              title: title,
-              link: link,
-              matchedKeyword: word
-            }
-            console.log(match);
+      for (const word of titleKeywords) {
+        if (keywords[word] === true) {
+          console.log("Found a match");
+          const match = {
+            title: title,
+            link: link,
+            matchedKeyword: word
           }
+          console.log(match);
         }
-      });
-    })
-  };
+      }
+    });
+  })
 };
 
 Parser.getArtists('./wantlist/wantlist.csv')
