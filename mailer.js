af3fdb30733c4cf1d5955f9f1e149b65927a6616
Haakasaurus <kramerhjohnson@gmail.com>
2020-05-20 17:27:38 -0700

    Added dotenv and smtpTransport init

--- mailer.js
+++ mailer.js
@@ -1,3 +1,13 @@
+require("dotenv").config();
 const Parser = require("parser");
 const Scraper = require("scrape");
 const nodemailer = require("nodemailer");
+
+smtpTransport = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
+    auth: {
+        user: "haaken1234@gmail.com",
+        pass: process.env.GMAIL
+    }
+})
+
